2c5436648fb07a1bd111c29dd395bc48b2371d57
spotter002 <spotterwamai@gmail.com>
2025-08-18 12:27:30 +0300

    port

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`)
 })
